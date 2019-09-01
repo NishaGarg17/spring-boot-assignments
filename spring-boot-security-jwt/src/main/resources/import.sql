cbbc4746f2408f6d4b9fd9c14600ba1d655956ff
Nisha Garg <14gargnisha@gmail.com>
2019-09-01 23:44:08 +0530

    intial data to db loaded successfully using spring boot hibernate

--- spring-boot-security-jwt/src/main/resources/import.sql
+++ spring-boot-security-jwt/src/main/resources/import.sql
@@ -6,5 +6,6 @@ insert into `user`(email,username,enabled,password) values('john@gmail.com','Joh
 insert into `user`(email,username,enabled,password) values('anil@gmail.com','Anil',1,'{noop}manager');
 insert into `user`(email,username,enabled,password) values('nisha@gmail.com','Nisha',1,'{noop}employee');
 
-
-
+insert into `user_authorities`(users_id, authorities_id) values((select id from user where email = 'john@gmail.com'), (select id from authority where name = 'ROLE_ADMIN'))
+insert into `user_authorities`(users_id, authorities_id) values((select id from user where email = 'anil@gmail.com'), (select id from authority where name = 'ROLE_MANAGER'))
+insert into `user_authorities`(users_id, authorities_id) values((select id from user where email = 'nisha@gmail.com'), (select id from authority where name = 'ROLE_EMPLOYEE'))
