223e91c034d2de986cac2dbac40bb3970df81f8a
Nisha Garg <14gargnisha@gmail.com>
2019-09-03 19:46:24 +0530

    sign up link working

--- spring-boot-security-jwt/src/main/java/com/springboot/assignments/springbootsecurityjwt/controller/DemoController.java
+++ spring-boot-security-jwt/src/main/java/com/springboot/assignments/springbootsecurityjwt/controller/DemoController.java
@@ -1,6 +1,9 @@
 package com.springboot.assignments.springbootsecurityjwt.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.springboot.assignments.springbootsecurityjwt.entity.User;
 
 @Controller
 public class DemoController {
@@ -21,6 +24,12 @@ public class DemoController {
 	@GetMapping("/systems")
 	public String showAdminForum() {
 		return "systems";
+	}
+	
+	@GetMapping("/sign-up")
+	public String showSignUpPage(Model model) {
+		model.addAttribute("user", new User());
+		return "sign-up";
 	}
 	
 }
