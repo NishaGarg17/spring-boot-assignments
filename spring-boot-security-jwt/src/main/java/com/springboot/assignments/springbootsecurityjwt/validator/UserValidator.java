fad6a553e733de2f74404a2e000d70effe37375a
Nisha Garg <14gargnisha@gmail.com>
2019-09-11 20:10:01 +0530

    password policy completed

--- spring-boot-security-jwt/src/main/java/com/springboot/assignments/springbootsecurityjwt/validator/UserValidator.java
+++ spring-boot-security-jwt/src/main/java/com/springboot/assignments/springbootsecurityjwt/validator/UserValidator.java
@@ -49,26 +49,17 @@ public class UserValidator implements Validator{
 				errors.rejectValue("passwordConfirm", "different.user.passwordConfirm");
 			}
 			
-			// pattern to compile allowed length for password
-//			String passwordPolicy = "((?=.*[a-z])(?=.*d)(?=.*[@#$%])(?=.*[A-Z]))";
-//			Pattern passwordLengthPattern = Pattern.compile("(passwordPolicy.*{5,15})");
-//			Matcher passwordLengthMatcher = passwordLengthPattern.matcher(user.getPassword());
-			
-			// check if password follows minimum and maximum allowed length
-			// check that password must contain at least one digit, one special character,
-			// at least one lower case alphabet, at lease one upper case alphabet
-			// and must be of minimum 5 length and max 15
-			
-			Pattern passwordPolicyPattern = Pattern.compile("(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%])(?=.*[A-Z])");
+			Pattern passwordPolicyPattern = Pattern.compile("((?=.*[a-z])(?=.*[0-9])(?=.*[@#$%])(?=.*[A-Z])).*");
 			Matcher passwordPolicymatcher = passwordPolicyPattern.matcher(user.getPassword());
-			if(!passwordPolicymatcher.matches()) {
+			
+			
+			if(user.getPassword().length()<5 || user.getPassword().length()>15) {
+			  errors.rejectValue("password","size.policy.password"); }
+			else if(!passwordPolicymatcher.matches()) {
 				System.out.println("policy not followed" + user.getPassword());
 				errors.rejectValue("password", "criteria.policy.password");
 			}
-			/*
-			 * else if(!passwordLengthMatcher.matches()) {
-			 * errors.rejectValue("password","size.policy.password"); }
-			 */ 
+			 
 		}
 		
 		
